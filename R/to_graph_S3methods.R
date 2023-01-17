4ccfb2d8b3eecd47905f5a50e556ba3ddebb496e
Luigi Augugliaro <luigi.augugliaro@unipa.it>
2023-01-17 16:00:15 +0000

    version 2.0.6

--- R/to_graph_S3methods.R
+++ R/to_graph_S3methods.R
@@ -3,6 +3,8 @@ is.cglasso2igraph <- function(x) inherits(x, 'cglasso2igraph')
 print.cglasso2igraph <- function(x, ...) print.listof(x, ...)
 
 plot.cglasso2igraph <- function(x, type, ...){
+    op <- par(no.readonly = TRUE)
+    on.exit(par(op), add = TRUE)
     if(missing(type)) type <- ifelse(is.null(x$Gxy), "Gyy", "both")
     gr <- getGraph(x, type = type)
     opt <- list(...)
